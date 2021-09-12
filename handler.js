592975ae71c880eb6b94d47d07048185d52772f4
sirchras <cjlynch@hotmail.co.nz>
2021-09-13 10:34:02 +1200

    wip: rewrite to use param store

--- handler.js
+++ handler.js
@@ -1,21 +1,33 @@
+const aws = require('aws-sdk')
 const backup = require("./backup")
 
 require("dotenv").config()
 
 module.exports.runBackup = (event, context, callback) => {
-  const options = {
-    githubAccessToken: process.env.GITHUB_ACCESS_TOKEN,
-    s3BucketName: process.env.AWS_S3_BUCKET_NAME,
-    s3AccessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    s3AccessSecretKey: process.env.AWS_S3_ACCESS_SECRET_KEY,
-    s3StorageClass: process.env.AWS_S3_STORAGE_CLASS,
-    mode: process.env.BACKUP_MODE,
-    organisation: process.env.GITHUB_ORGANISATION
+  const ssm = new aws.SSM()
+  const param = {
+    Name: 'GITHUB_PAT',
+    WithDecryption: true
   }
 
-  backup(options).then(() => {
-    callback(null, {
-      response: "all repos was succesfully backed up"
+  ssm.getParameter(param).promise()
+    .then(res => {
+      const { Parameter: { Value: githubAccessToken } } = res
+
+      return {
+        githubAccessToken,
+        s3BucketName: process.env.AWS_S3_BUCKET_NAME,
+        s3AccessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+        s3AccessSecretKey: process.env.AWS_S3_ACCESS_SECRET_KEY,
+        s3StorageClass: process.env.AWS_S3_STORAGE_CLASS,
+        mode: process.env.BACKUP_MODE,
+        organisation: process.env.GITHUB_ORGANISATION
+      }
     })
-  }, callback)
+    .then(options => backup(options))
+    .then(() => {
+      callback(null, {
+        response: "all repos were successfully backed up"
+      })
+    })
 }
